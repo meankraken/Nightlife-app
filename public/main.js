696bf0c3471b428eefea0e87da958fb7ed824437
Jeff <kraken1337@yahoo.com>
2016-08-29 23:51:31 -0500

    Updating

--- public/main.js
+++ public/main.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+$(document).ready(function() {
+	var timer;
+	$('.searchBox').hover(function() {
+		clearTimeout(timer);
+		$(this).css("width","250px");
+		$(this).attr("placeholder"," ");
+		$(this).css("border-color","#A3DAE3");
+		$(this).css("box-shadow","0px 0px 5px 1px #A5F1F2");
+	}, function() {
+		$(this).css("box-shadow","");
+		if (!$('.searchBox').is(":focus")) {
+			timer = setTimeout(function() {
+				$(this).css("border-color","#1671CC");
+				$(this).css("width","30px");
+				$(this).attr("placeholder", "GO");
+			}.bind(this),500);
+		}	
+	});
+	
+	$('.searchBox').focusout(function() {
+		$(this).css("border-color","#1671CC");
+		$(this).css("width","30px");
+		$(this).attr("placeholder", "GO");
+		$(this).css("box-shadow","");
+	}); 
+	
+});
 
 class App extends React.Component {
 	constructor(props) {
@@ -11,4 +40,4 @@ class App extends React.Component {
 	}
 }
 
-ReactDOM.render(<App/>, document.querySelector(".app"));
+//ReactDOM.render(<App/>, document.querySelector(".app"));
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
 
 var app = express();
 
