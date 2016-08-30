1f94646b486c86e11cbb896f8673764cb3953a27
Jeff <kraken1337@yahoo.com>
2016-08-29 20:22:52 -0500

    Updating

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ var port = process.env.port || 8080;
 
 var app = express();
 
+app.set('views', process.cwd() + '/views' );
+app.set('view engine', 'jade');
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/build', express.static(process.cwd() + '/build'));
+
 app.get('/', function(req,res) {
-	res.end("test"); 
+	res.render('index');
 	
 });
 
